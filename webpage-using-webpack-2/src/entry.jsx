984e6912d02aa7cb52dc49f398ee4d762aa4247b
Domas Petkevicius <dpet@dbml.dk>
2017-07-04 23:12:41 +0300

    Add webpage using webpack 2

---
+++ webpage-using-webpack-2/src/entry.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => (
+  <div>
+    <img src="https://m.popkey.co/5b03a0/WWqAa.gif" />
+  </div>
+);
+
+render(<App />, document.querySelector('#app'));
